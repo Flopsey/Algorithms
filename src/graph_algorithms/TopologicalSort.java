09ed40d9f661fb5186ac8343985de253bee862c9
Flopsey <25252925+Flopsey@users.noreply.github.com>
2021-01-25 09:04:58 +0100

    Simplify topological sort
    Every “post” value is only updated once. Therefore the vertices can be directly prepended to a list.

--- src/graph_algorithms/TopologicalSort.java
+++ src/graph_algorithms/TopologicalSort.java
@@ -20,9 +20,8 @@ public class TopologicalSort {
     public static LinkedList<Integer> topologicalSort(GraphUnweighted graph) {
         Stack<Integer> stack = new LinkedListStack<>();
         LinkedList<Integer> visited = new LinkedList<>();  // TODO: Use Dictionary once AVLTree is finished
+        LinkedList<Integer> topo = new LinkedList<>();
         int vertexCount = graph.getVertexCount();
-        Heap<PriorityQueue.KeyValuePair<Integer>> post = new Heap<>();
-        int t = 0;
         for (int s = 0; s < vertexCount; ++s) {
             if (visited.contains(s)) {
                 continue;
@@ -32,11 +31,7 @@ public class TopologicalSort {
                 int u = stack.top();
                 if (visited.contains(u)) {
                     stack.pop();
-                    if (post.contains(u)) {
-                        post.decreaseKey(u, t--);
-                    } else {
-                        post.insert(new PriorityQueue.KeyValuePair<>(u, t++));
-                    }
+                    topo.addFirst(u);
                 } else {
                     visited.addLast(u);
                     LinkedList<Integer> out = graph.outEdges(u);
@@ -48,10 +43,6 @@ public class TopologicalSort {
                     }
                 }
             }
-        }
-        LinkedList<Integer> topo = new LinkedList<>();
-        while (post.getMin() != null) {
-            topo.addFirst(post.extractMin().value);
         }
         return topo;
     }
