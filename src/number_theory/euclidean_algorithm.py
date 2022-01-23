e8276e8b1ebfbd6fa3b241bca1eaea95765add9c
Florian Affolter <faffolter@student.ethz.ch>
2022-01-23 22:35:59 +0100

    Add least common multiple

--- src/number_theory/euclidean_algorithm.py
+++ src/number_theory/euclidean_algorithm.py
@@ -5,3 +5,7 @@ def gcd(a: Integral, b: Integral) -> Integral:
     while b != 0:
         a, b = b, a % b
     return a
+
+
+def lcm(a: Integral, b: Integral) -> Integral:
+    return (a * b) // gcd(a, b)
